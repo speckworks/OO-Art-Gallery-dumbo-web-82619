a300c4539dfbbb2921a989478eaa4be176a2815e
John Karrell Speck <speckworks@gmail.com>
2019-09-09 14:11:40 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,29 @@
 class Artist
-
   attr_reader :name, :years_experience
-
+  @@all = []
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all<< self
   end
 
+  def self.all  
+   @@all
+  end
+
+  def paintings
+    Painting.all.select {|painting| painting.artist == self.name}
+  end
+  
+  def galleries
+    self.paintings.map {|painting| painting.gallery}
+  end
+
+  def cities 
+    galleries.map {|gallery| gallery.city}
+  end
+  
+  def self.total_experience
+    Artist.years_experience.map {|years_experience| Artist.years_experience.reduce(:+)}
+  end
 end
